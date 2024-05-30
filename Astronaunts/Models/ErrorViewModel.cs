b69d841e66dd304e9b2342b2413bf42264896659
Yahel <yahel631@gmail.com>
2024-05-30 12:19:30 -0600

    se realizo un cambio en ErrorViewModel.cs

---
+++ Astronaunts/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Astronaunts.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
