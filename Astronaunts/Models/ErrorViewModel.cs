5041ff40360a478a026c10ee6055e3e0ae03c911
Yahel <yahel631@gmail.com>
2024-06-04 08:04:34 -0600

    Revert "se realizo un cambio en ErrorViewModel.cs"
    This reverts commit b69d841e66dd304e9b2342b2413bf42264896659.

--- Astronaunts/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Astronaunts.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
